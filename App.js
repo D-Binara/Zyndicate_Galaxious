c4eadb4aef0b860be9f4f2fd351ecfc456c68cf6
D_Binara <dbkaveesha@gmail.com>
2023-08-16 01:03:54 +0530

    First update

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import s1 from './Src/Screen/s1';
+
+
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <s1></s1>
   );
 }
 
---
+++ Src/Screen/s1.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { SafeAreaView, ScrollView ,StyleSheet} from "react-native";
+
+const s1 = ()=> {
+    return(
+        <SafeAreaView>
+            <ScrollView>
+
+            </ScrollView>
+        </SafeAreaView>
+    )
+
+const styles = StyleSheet.create ({
+
+});
+
+
+}
+
+export default s1;
