f89ef087613da7d43e32a356c7af1f3f3af21dc6
D_Binara <dbkaveesha@gmail.com>
2023-08-18 19:34:19 +0530

    update

--- App.js
+++ App.js
@@ -2,17 +2,33 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import s1 from './Src/Screen/s1';
+import S1 from './Src/Screen/s1';
+import S2 from './Src/Screen/s2';
 
+const Stack = createNativeStackNavigator();
 
-
-
-export default function App() {
+const App = () => {
   return (
-    <s1></s1>
+    <NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen
+          name="s2"
+          component={S2}
+          options={{title: 's2'}}
+        />
+        <Stack.Screen name="s1" component={S1} />
+      </Stack.Navigator>
+    </NavigationContainer>,
+     <S1></S1>,
+     <S2></S2>
   );
-}
+};
+
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
--- Src/Screen/s1.js
+++ Src/Screen/s1.js
@@ -1,20 +1,69 @@
-import React from "react";
-import { SafeAreaView, ScrollView ,StyleSheet} from "react-native";
+import * as React from 'react';
+import { SafeAreaView, ScrollView ,StyleSheet,View,Text,Button, ImageBackground,Dimensions} from "react-native";
 
-const s1 = ()=> {
+const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get('window').width;
+
+
+const S1 = ({ navigation })=> {
     return(
-        <SafeAreaView>
+        <SafeAreaView style={{flex:1}} >
             <ScrollView>
-
+                     <View style ={styles.container}>
+                        <ImageBackground source={require('../Images/S1BG.png')} resizeMode="cover" style={styles.image}>
+                        <Text style={styles.myTitle}>Expore The {'\n'}Universe</Text>
+                        <Text style={styles.description}>The universe is all of space and time and their contents, including planets, stars, galaxies, and all other forms of matter and energy.</Text>
+                        <View style={styles.buttonex}>
+                            <Button 
+                                title="Explore"
+                                onPress={() => navigation.navigate('s2')}
+                            />
+                        </View>
+                        </ImageBackground>
+                    </View> 
             </ScrollView>
         </SafeAreaView>
     )
-
+    }
 const styles = StyleSheet.create ({
-
+    buttonex:{
+        paddingTop:75,
+        paddingLeft:65,
+        color:'#2196F3',
+        alignItems:'center',
+        elevation:8,
+        borderRadius: 10,
+        paddingVertical: 0,
+        paddingHorizontal: 12
+    },
+    image: {
+        height: screenHeight,
+        width: screenWidth,
+        justifyContent: 'center',
+        alignItems: 'Right',
+        paddingRight:60,
+    },
+    description:{
+        fontFamily:'Roboto',
+        color:'white',
+        textAlign:'justify',
+        fontSize:13,
+        paddingRight:25,
+        paddingLeft:50,
+    },
+    myTitle:{
+        fontFamily:'secular one',
+        fontSize:35,
+        color:'white',
+        fontWeight:'bold',
+        paddingTop:300,
+        paddingRight:100,
+        paddingLeft:50,
+    },
+    container: {
+        paddingBottom:150,
+        flex: 1,
+    },
 });
 
-
-}
-
-export default s1;
+export default S1;
