b85386bc56f9d6798d3b7f0516ec518c7982bf17
D_Binara <dbkaveesha@gmail.com>
2023-08-20 00:17:13 +0530

    Update

--- Src/Screen/Plane2.js
+++ Src/Screen/Plane2.js
@@ -1,94 +1,147 @@
 import * as React from 'react';
-import { SafeAreaView, ScrollView ,StyleSheet,View,Text,Button, ImageBackground,Dimensions} from "react-native";
+import { SafeAreaView, ScrollView, StyleSheet, View, Text, Button, ImageBackground, Dimensions } from "react-native";
 
 
 const screenHeight = Dimensions.get('window').height;
 const screenWidth = Dimensions.get('window').width;
 
 
-const Plane2 = ({})=> {
-    return(
-        <SafeAreaView style={{flex:1}} >
+const Plane2 = ({ navigation }) => {
+    return (
+        <SafeAreaView style={{ flex: 1 }} >
             <ScrollView>
-                     <View style ={styles.container}>
-         
-                        <ImageBackground source={require('../Images/Plan2bg.png')} resizeMode="cover" style={styles.image}>
+                <View style={styles.container}>
+
+                    <ImageBackground source={require('../Images/Plan2bg.png')} resizeMode="cover" style={styles.image}>
                         <Text style={styles.moon}>Moon</Text>
+
                         <Text style={styles.description1}>The moon is Earth's natural satellite and the only other celestial body that humans  have visited.
-                         It has a barren   and rocky landscape with  numerous craters and.                            
-                       </Text>
-                       <Text style={styles.description2}>
-                       Start your day with a visit to the  SPACE station 
-                       </Text>
-                        <View style={styles.buttonex}>
-                        <Button 
-                               onPress={() => navigation.navigate('s1')}
-                               title="Book Now"
+                            It has a barren   and rocky landscape with  numerous craters and.
+                        </Text>
+
+                        <View style={styles.squarelocate}>
+                            <View style={styles.square} >
+                                <Text style={styles.day} >Day 1</Text>
+
+                            </View>
+                        </View>
+
+                        <Text style={styles.Sentense}>
+                            Start your day with a visit to the  SPACE station
+                        </Text>
+
+                        <View style={styles.buttonbooknow}>
+                            <Button
+                                onPress={() => navigation.navigate('S1')}
+                                title="Book Now"
                             />
                         </View>
                         <Text style={styles.description2}>
-                        mountains. It lacks an  atmosphere and experiences extreme temperatures. 
-                        It's a fascinating destination  for space exploration and offers  breathtaking views of Earth  from its surface
+                            mountains. It lacks an  atmosphere and experiences extreme temperatures.
+                            It's a fascinating destination  for space exploration and offers  breathtaking views of Earth  from its surface
                         </Text>
-                        <Text>Find more details</Text>
-                        <Button 
-                               onPress={() => navigation.navigate('s1')}
-                               title="View"
+
+                        <Text style={styles.Findmore}>Find more details</Text>
+                        
+                        <View style={styles.buttonview}>
+                            <Button
+                                onPress={() => navigation.navigate('s1')}
+                                title="View"
                             />
-                        </ImageBackground>
-                    </View> 
+                        </View>
+
+                    </ImageBackground>
+                </View>
             </ScrollView>
         </SafeAreaView>
     )
-    }
-    const styles = StyleSheet.create ({
-        container: {
-            flex: 1,
-        },
-        moon:{
-            fontFamily:'secular one',
-            fontSize:55,
-            color:'white',
-            fontWeight:'bold',
-            paddingBottom:20,
-            paddingRight:100,
-            paddingLeft:50,
-        },
-        description1:{
-            fontFamily:'Roboto',
-            color:'white',
-            textAlign:'justify',
-            fontSize:13,
-            paddingBottom:630,
-            paddingRight:25,
-            paddingLeft:50,
-        },
-        buttonex:{
-            paddingTop:75,
-            paddingLeft:65,
-            color:'#2196F3',
-            alignItems:'center',
-            elevation:8,
-            borderRadius: 10,
-            paddingVertical: 0,
-            paddingHorizontal: 12
-        },
-        image: {
-            height: 1500,
-            width: screenWidth,
-            justifyContent: 'center',
-            alignItems: 'Right',
-            paddingRight:60,
-        },
-        description2:{
-            fontFamily:'Roboto',
-            color:'white',
-            textAlign:'justify',
-            fontSize:13,
-            paddingRight:25,
-            paddingLeft:50,
-        },
-
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    moon: {
+        fontFamily: 'secular one',
+        fontSize: 55,
+        color: 'white',
+        fontWeight: 'bold',
+        paddingBottom: 20,
+        paddingRight: 100,
+        paddingLeft: 50,
+    },
+    description1: {
+        fontFamily: 'Roboto',
+        color: 'white',
+        textAlign: 'justify',
+        fontSize: 13,
+        paddingBottom: 50,
+        paddingRight: 25,
+        paddingLeft: 50,
+    },
+    squarelocate: {
+        paddingBottom: 30,
+        paddingRight: 25,
+        paddingLeft: 50,
+    },
+    square: {
+        width: 90,
+        height: 40,
+        backgroundColor: "yellow",
+    },
+    day: {
+        color: 'black',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 20
+    },
+    Sentense: {
+        paddingBottom: 430,
+        paddingLeft: 50,
+        fontFamily: 'Roboto',
+        color: 'white',
+    },
+    buttonbooknow: {
+        paddingTop: 75,
+        paddingLeft: 65,
+        color: '#2196F3',
+        alignItems: 'center',
+        elevation: 8,
+        borderRadius: 10,
+        paddingVertical: 0,
+        paddingHorizontal: 12
+    },
+    image: {
+        height: 1500,
+        width: screenWidth,
+        justifyContent: 'center',
+        alignItems: 'Right',
+        paddingRight: 60,
+    },
+    description2: {
+        fontFamily: 'Roboto',
+        color: 'white',
+        textAlign: 'justify',
+        fontSize: 13,
+        paddingRight: 25,
+        paddingLeft: 50,
+    },
+    Findmore:{
+        paddingBottom: 430,
+        paddingLeft: 50,
+        fontFamily: 'Roboto',
+        color: 'white',
+        alignItems: 'center',
+    },
+    buttonview: {
+        paddingTop: 25,
+        paddingLeft: 65,
+        color: '#2196F3',
+        alignItems: 'center',
+        elevation: 8,
+        borderRadius: 10,
+        paddingVertical: 0,
+        paddingHorizontal: 12
+    },
 })
 
 export default Plane2;
--- Src/Screen/s1.js
+++ Src/Screen/s1.js
@@ -1,35 +1,35 @@
 import * as React from 'react';
-import { SafeAreaView, ScrollView ,StyleSheet,View,Text,Button, ImageBackground,Dimensions} from "react-native";
+import { SafeAreaView, ScrollView, StyleSheet, View, Text, Button, ImageBackground, Dimensions } from "react-native";
 
 const screenHeight = Dimensions.get('window').height;
 const screenWidth = Dimensions.get('window').width;
 
-const S1 = ({ navigation })=> {
+const S1 = ({ navigation }) => {
     return (
-        <SafeAreaView style={{flex:1}} >
-                 <View style ={styles.container}>
-                    <ImageBackground source={require('../Images/S1BG.png')} resizeMode="cover" style={styles.image}>
+        <SafeAreaView style={{ flex: 1 }} >
+            <View style={styles.container}>
+                <ImageBackground source={require('../Images/S1BG.png')} resizeMode="cover" style={styles.image}>
                     <Text style={styles.myTitle}>Expore The {'\n'}Universe</Text>
                     <Text style={styles.description}>The universe is all of space and time and their contents, including planets, stars, galaxies, and all other forms of matter and energy.</Text>
                     <View style={styles.buttonex}>
-                        <Button 
+                        <Button
                             title="Explore"
                             onPress={() => navigation.navigate('S2')}
                         />
                     </View>
-                    </ImageBackground>
-                </View> 
-    </SafeAreaView>
+                </ImageBackground>
+            </View>
+        </SafeAreaView>
     );
-  }
+}
 
-const styles = StyleSheet.create ({
-    buttonex:{
-        paddingTop:75,
-        paddingLeft:65,
-        color:'#2196F3',
-        alignItems:'center',
-        elevation:8,
+const styles = StyleSheet.create({
+    buttonex: {
+        paddingTop: 75,
+        paddingLeft: 65,
+        color: '#2196F3',
+        alignItems: 'center',
+        elevation: 8,
         borderRadius: 10,
         paddingVertical: 0,
         paddingHorizontal: 12
@@ -39,27 +39,27 @@ const styles = StyleSheet.create ({
         width: screenWidth,
         justifyContent: 'center',
         alignItems: 'Right',
-        paddingRight:60,
+        paddingRight: 60,
     },
-    description:{
-        fontFamily:'Roboto',
-        color:'white',
-        textAlign:'justify',
-        fontSize:13,
-        paddingRight:25,
-        paddingLeft:50,
+    description: {
+        fontFamily: 'Roboto',
+        color: 'white',
+        textAlign: 'justify',
+        fontSize: 13,
+        paddingRight: 25,
+        paddingLeft: 50,
     },
-    myTitle:{
-        fontFamily:'secular one',
-        fontSize:35,
-        color:'white',
-        fontWeight:'bold',
-        paddingTop:300,
-        paddingRight:100,
-        paddingLeft:50,
+    myTitle: {
+        fontFamily: 'secular one',
+        fontSize: 35,
+        color: 'white',
+        fontWeight: 'bold',
+        paddingTop: 300,
+        paddingRight: 100,
+        paddingLeft: 50,
     },
     container: {
-        paddingBottom:150,
+        paddingBottom: 150,
         flex: 1,
     },
 });
